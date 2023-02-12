124a43aec8aab259a00c950ef0d401cdec6a5f29
Clayton Rafael <clayton.rssouza@gmail.com>
2023-02-12 09:32:10 -0300

    chore: apply code style

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import express, { Application, Request, Response } from "express";
-import { router as userRoutes } from "./routes/user.routes";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express'
+import { router as userRoutes } from './routes/user.routes'
+import cors from 'cors'
 
-const app: Application = express();
+const app: Application = express()
 
-app.use("/", userRoutes);
-app.use(cors());
+app.use('/', userRoutes)
+app.use(cors())
 
-app.use("/", (req: Request, res: Response): void => {
-  res.json({ message: "All right" });
-});
+app.use('/', (req: Request, res: Response): void => {
+  res.json({ message: 'All right' })
+})
 
-export default app;
+export default app
